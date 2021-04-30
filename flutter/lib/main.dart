683ae3a883f3ce4c6f73e83e88198d02e777e208
Matheus Vanzan <vanzan2015@gmail.com>
2021-04-30 10:29:02 -0300

    init

---
+++ flutter/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:location/location.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Location location = new Location();
+  LocationData _locationData;
+
+  void getDeviceInfo() {
+    print('Info dispositivo');
+    print('Flutter');
+
+
+    print('Canal iOS');
+
+
+    print('Canal Android');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getDeviceInfo();
+    
+    // Código para atualizar a localização ao se mover
+    // location.onLocationChanged.listen((LocationData loc) {
+    //   setState(() {
+    //     _locationData = loc;
+    //   });
+    // });
+
+    // poderia usar um canal para obter a localização
+
+    location.getLocation()
+    .then((LocationData loc) {
+      setState(() {
+        _locationData = loc;
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FlutterMap(
+        options: new MapOptions(
+          center: new LatLng(_locationData.latitude, _locationData.longitude),
+          zoom: 13.0,
+        ),
+        layers: [
+          new TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']
+          ),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 80.0,
+                height: 80.0,
+                point: new LatLng(_locationData.latitude, _locationData.longitude),
+                builder: (ctx) =>
+                new Container(
+                  child: new Image.network('https://www.jav.com.br/wp-content/uploads/2017/03/map-marker-icon.png'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
---
+++ ios/mapas/mapas/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  mapas
+//
+//  Created by Matheus Vanzan on 26/04/21.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDelegate {
+
+    let mapView = MKMapView()
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Mapa
+        view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        mapView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        
+        // Acesso ao GPS
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenter(coor, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+
+        mapView.mapType = MKMapType.standard
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+//        annotation.title = "Você"
+//        annotation.subtitle = "current location"
+        mapView.addAnnotation(annotation)
+    }
+}
